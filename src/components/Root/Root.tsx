a8bc11ac6425c4446b676211fa467a8f15966bff
Daniil <ksusonic@yandex.ru>
2025-10-11 21:18:00 +0300

    feat: Refactor layout and page structure for Telegram Mini App using tma

---
+++ src/components/Root/Root.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { type PropsWithChildren, useEffect } from 'react';
+import {
+  initData,
+  miniApp,
+  useLaunchParams,
+  useSignal,
+} from '@telegram-apps/sdk-react';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { AppRoot } from '@telegram-apps/telegram-ui';
+
+import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { ErrorPage } from '@/components/ErrorPage';
+import { useDidMount } from '@/hooks/useDidMount';
+
+import './styles.css';
+
+function RootInner({ children }: PropsWithChildren) {
+  const lp = useLaunchParams();
+
+  const isDark = useSignal(miniApp.isDark);
+  const initDataUser = useSignal(initData.user);
+
+  useEffect(() => {
+  }, [initDataUser]);
+
+  return (
+    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+      <AppRoot
+        appearance={isDark ? 'dark' : 'light'}
+        platform={
+          ['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'
+        }
+      >
+        {children}
+      </AppRoot>
+    </TonConnectUIProvider>
+  );
+}
+
+export function Root(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of
+  // the Server Side Rendering. That's why we are showing loader on the server
+  // side.
+  const didMount = useDidMount();
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props} />
+    </ErrorBoundary>
+  ) : (
+    <div className="root__loading">Loading</div>
+  );
+}
--- src/types/telegram.ts
+++
@@ -1,7 +0,0 @@
-export interface TelegramUser {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code?: string;
-}
