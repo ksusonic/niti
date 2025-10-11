353eb40cb8b95bb09e61af2d58de35dffadc81eb
Daniil <ksusonic@yandex.ru>
2025-10-11 03:59:57 +0300

    Single app (#46)
    * Remove all
    
    * Fullstack Telegram web app
    
    * Remove public
    
    * Refactor event participant handling and update schema to use Number type

--- backend/internal/storage/repository/users/delete.go
+++
@@ -1,8 +0,0 @@
-package users
-
-import "context"
-
-func (r *Repository) Delete(ctx context.Context, telegramID int64) error {
-	_, err := r.Exec(ctx, `DELETE FROM users WHERE telegram_id = $1`, telegramID)
-	return err
-}
--- backend/pkg/config/telegram.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type TelegramConfig struct {
-	Verbose   bool   `env:"TELEGRAM_BOT_VERBOSE"`
-	Token     string `env:"TELEGRAM_BOT_TOKEN"`
-	WebAppUrl string `env:"TELEGRAM_BOT_WEBAPP_URL" envDefault:"https://example.com"`
-}
