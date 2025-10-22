3ea0db39def64c8f1ff3c6bd0cf5212eb019265e
Daniil <ksusonic@yandex.ru>
2025-10-22 22:44:46 +0300

    Review fixes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ? parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE) : 1,
 
   // Enable logs to be sent to Sentry
   enableLogs: true,
