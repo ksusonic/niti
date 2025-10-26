5a76e6d25a321c0373c4375b656d33a100478ab0
Daniil <ksusonic@yandex.ru>
2025-10-27 00:12:38 +0300

    Minor fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ['grammy'],
-	},
 	images: {
 		dangerouslyAllowLocalIP: true,
 		remotePatterns: [
